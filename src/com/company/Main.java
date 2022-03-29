c053841c67facd31cac76b0da5126d3edc72931f
Şahin <sahincakmak7@gmail.com>
2022-03-29 15:28:47 +0300

    Ödev1 v2.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int point;
-        int passGrade = 60;
+        int matematik, fizik, kimya, türkçe, tarih, muzik;
 
         Scanner input = new Scanner(System.in);
+        System.out.print("Matematik Dersinizin Notunu Giriniz: ");
+        matematik = input.nextInt();
+        System.out.print("Fizik Dersinizin Notunu Giriniz: ");
+        fizik = input.nextInt();
+        System.out.print("Kimya Dersinizin Notunu Giriniz: ");
+        kimya = input.nextInt();
+        System.out.print("Türkçe Dersinizin Notunu Giriniz: ");
+        türkçe = input.nextInt();
+        System.out.print("Tarih Dersinizin Notunu Giriniz: ");
+        tarih = input.nextInt();
+        System.out.print("Müzik Dersinizin Notunu Giriniz: ");
+        muzik = input.nextInt();
 
-        System.out.print("Notunuzu giriniz :");
-        point = input.nextInt();
-        boolean condition = point < passGrade;
+        double result = ((matematik + fizik + kimya + türkçe + tarih + muzik) / 6);
+        System.out.println("Ortalamanız : " + result);
 
-        String result = condition ? "Kaldınız" : "Geçtiniz";
-        System.out.println(result);
+        String str = result >= 60 ? "Tebrikler geçtiniz. " : "Üzgünüm kaldınız.";
+        System.out.print(str);
+
+
     }
-
-
-    }
-
+}
 
 
